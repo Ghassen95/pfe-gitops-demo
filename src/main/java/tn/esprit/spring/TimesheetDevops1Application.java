ed7d5e61b10204ba769a5f231b5f6c577f8e79f1
Ghassen95 <ghassen.rouissi95@gmail.com>
2022-06-30 10:23:00 +0100

    change source code

---
+++ src/main/java/tn/esprit/spring/TimesheetDevops1Application.java
@@ -0,0 +1,31 @@
+package tn.esprit.spring;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+import tn.esprit.spring.services.UserServiceImpl;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+
+public class TimesheetDevops1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimesheetDevops1Application.class, args);
+	}
+	UserServiceImpl us=null;
+	String string = "2022-04-04";
+	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+	LocalDate date = LocalDate.parse(string, formatter);
+
+	//User u = new User("Ali","Ben ahmed",date,Role.ADMINISTRATEUR);
+	//User userAdded = us.addUser(u);
+
+	
+	
+
+}
